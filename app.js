b22681dcd8c0289e4a15a69a3fb03b9b951e5c71
gitcoder <kr.sandeep.mr@gmail.com>
2017-09-23 17:58:24 +0530

    added interactivity and some further styling for content

--- app.js
+++ app.js
@@ -18,4 +18,19 @@ $(document).ready(function() {
          event.preventDefault();
          $('.js-form').toggleClass('is-visible');
   });
+
+  // Modal
+   
+   $('.js-show-modal').on('click', function(event) {
+         event.preventDefault();
+         $('.js-modal').addClass('is-visible');
+         $('.js-modal-overlay').addClass('is-visible');
+  });
+  
+   $('.js-modal-overlay').on('click', function(event) {
+         
+         $('.js-modal').removeClass('is-visible');
+         $('.js-modal-overlay').removeClass('is-visible');
+  });
+
 });
